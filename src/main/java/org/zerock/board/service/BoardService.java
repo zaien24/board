80656abd0af27271b3c01e94b8ea39b24d11d3f5
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-07 23:45:41 +0900

    p263 게시물 목록 처리

--- src/main/java/org/zerock/board/service/BoardService.java
+++ src/main/java/org/zerock/board/service/BoardService.java
@@ -1,12 +1,16 @@
 package org.zerock.board.service;
 
 import org.zerock.board.dto.BoardDTO;
+import org.zerock.board.dto.PageRequestDTO;
+import org.zerock.board.dto.PageResultDTO;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.Member;
 
 public interface BoardService {
 
     Long register(BoardDTO dto);
+
+    PageResultDTO<BoardDTO, Object[]> getList(PageRequestDTO pageRequestDTO);
 
     default Board dtoToEntity(BoardDTO dto){
 
@@ -19,6 +23,23 @@ public interface BoardService {
                 .writer(member)
                 .build();
         return board;
+    }
+
+    default BoardDTO entityToDTO(Board board, Member member, Long replyCount) {
+
+        BoardDTO boardDTO = BoardDTO.builder()
+                .bno(board.getBno())
+                .title(board.getTitle())
+                .content(board.getContent())
+                .regDate(board.getRegDate())
+                .modDate(board.getModDate())
+                .writerEmail(member.getEmail())
+                .writerName(member.getName())
+                .replyCount(replyCount.intValue()) //int로 처리하도록
+                .build();
+
+        return boardDTO;
+
     }
 
 }
