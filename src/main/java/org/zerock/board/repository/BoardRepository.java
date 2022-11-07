f56d1edbcb7c8582c571ea703d04d6b5b842481f
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-07 22:57:14 +0900

    p253 목록 화면에 필요한 JPQL  만들기

--- src/main/java/org/zerock/board/repository/BoardRepository.java
+++ src/main/java/org/zerock/board/repository/BoardRepository.java
@@ -1,5 +1,7 @@
 package org.zerock.board.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -15,4 +17,12 @@ public interface BoardRepository extends JpaRepository<Board, Long> {
 
     @Query("SELECT b, r FROM Board b LEFT JOIN Reply r ON r.board = b WHERE b.bno = :bno")
     List<Object[]> getBoardWithReply(@Param("bno") Long bno);
+
+    @Query(value = "SELECT b, w, count(r) " +
+                    " FROM Board b " +
+                    " LEFT JOIN b.writer w " +
+                    " LEFT JOIN Reply r ON r.board = b " +
+                    " GROUP BY b",
+                    countQuery = "SELECT count(b) FROM Board b")
+    Page<Object[]> getBoardWithReplyCount(Pageable pageable);
 }
