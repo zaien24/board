f38c39f5786bff65e14cfe754c5329fec6810175
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-20 00:06:29 +0900

    p298 - JQPLQUery n 객체

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,8 +1,12 @@
 package org.zerock.board.repository.search;
 
+import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.board.entity.Board;
+import org.zerock.board.entity.QBoard;
+
+import java.util.List;
 
 @Log4j2
 public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport implements SearchBoardRepository {
@@ -15,6 +19,20 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
     public Board search1() {
 
         log.info("search1........................");
+
+        QBoard board = QBoard.board;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+
+        jpqlQuery.select(board).where(board.bno.eq(1L));
+
+        log.info("---------------------------");
+        log.info(jpqlQuery);
+        log.info("---------------------------");
+
+        List<Board> result = jpqlQuery.fetch();
+
+        log.info(result);
 
         return null;
     }
