760d1ca13841ccd5d56dd1fb58fbf4a6610181cd
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-20 00:34:23 +0900

    P305  검색조건의 처리 bOOLEANeXPRESSION

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -1,6 +1,8 @@
 package org.zerock.board.repository.search;
 
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.JPQLQuery;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.data.domain.Page;
@@ -50,6 +52,52 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
     @Override
     public Page<Object[]> searchPage(String type, String keyword, Pageable pageable) {
         log.info("searchPage.............................");
+
+        QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
+        QMember member = QMember.member;
+
+        JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
+
+        //SELECT b, w, count(r) FROM Board b
+        //LEFT JOIN b.writer w LEFT JOIN Reply r ON r.board = b
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member, reply.count());
+
+        BooleanBuilder booleanBuilder = new BooleanBuilder();
+        BooleanExpression expression = board.bno.gt(0L);
+
+        booleanBuilder.and(expression);
+
+        if(type != null){
+            String[] typeArr = type.split("");
+            //검색 조건을 작성하기
+            BooleanBuilder conditionBuilder = new BooleanBuilder();
+
+            for (String t:typeArr) {
+                switch (t){
+                    case "t":
+                        conditionBuilder.or(board.title.contains(keyword));
+                        break;
+                    case "w":
+                        conditionBuilder.or(member.email.contains(keyword));
+                        break;
+                    case "c":
+                        conditionBuilder.or(board.content.contains(keyword));
+                        break;
+                }
+            }
+            booleanBuilder.and(conditionBuilder);
+        }
+
+        tuple.where(booleanBuilder);
+
+        tuple.groupBy(board);
+
+        List<Tuple> result = tuple.fetch();
+
+        log.info(result);
 
         return null;
     }
