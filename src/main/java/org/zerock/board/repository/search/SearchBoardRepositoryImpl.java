9c2a48f1ab0e8b2aa44267a00072ad9784a468b6
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-20 00:19:01 +0900

    p300 tuple 객체

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.QBoard;
+import org.zerock.board.entity.QMember;
 import org.zerock.board.entity.QReply;
 
 import java.util.List;
@@ -23,15 +24,20 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
 
         QBoard board = QBoard.board;
         QReply reply = QReply.reply;
+        QMember member = QMember.member;
 
         JPQLQuery<Board> jpqlQuery = from(board);
+        jpqlQuery.leftJoin(member).on(board.writer.eq(member));
         jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
 
+        JPQLQuery<Tuple> tuple = jpqlQuery.select(board, member.email, reply.count());
+        tuple.groupBy(board);
+
         log.info("---------------------------");
-        log.info(jpqlQuery);
+        log.info(tuple);
         log.info("---------------------------");
 
-        List<Board> result = jpqlQuery.fetch();
+        List<Tuple> result = tuple.fetch();
 
         log.info(result);
 
