394b1fc3e424d576414057b59b8a9aa16e10f325
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-20 00:13:21 +0900

    p299 JPQL Query 의 leftjOin / on()

--- src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
+++ src/main/java/org/zerock/board/repository/search/SearchBoardRepositoryImpl.java
@@ -5,6 +5,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
 import org.zerock.board.entity.Board;
 import org.zerock.board.entity.QBoard;
+import org.zerock.board.entity.QReply;
 
 import java.util.List;
 
@@ -21,10 +22,10 @@ public class SearchBoardRepositoryImpl extends QuerydslRepositorySupport impleme
         log.info("search1........................");
 
         QBoard board = QBoard.board;
+        QReply reply = QReply.reply;
 
         JPQLQuery<Board> jpqlQuery = from(board);
-
-        jpqlQuery.select(board).where(board.bno.eq(1L));
+        jpqlQuery.leftJoin(reply).on(reply.board.eq(board));
 
         log.info("---------------------------");
         log.info(jpqlQuery);
