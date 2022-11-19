9200017b4ecd8a4b3187fe735348c255df1bffd4
DESKTOP-9CTRFPU\zaien24 <zaien24@gmail.com>
2022-11-19 23:27:04 +0900

    p275  게시물 수정 처리

--- src/main/java/org/zerock/board/entity/Board.java
+++ src/main/java/org/zerock/board/entity/Board.java
@@ -23,5 +23,13 @@ public class Board extends BaseEntity {
     @ManyToOne (fetch = FetchType.LAZY)
     private Member writer;
 
+    public void changeTitle(String title){
+        this.title = title;
+    }
+
+    public void changeContent(String content){
+        this.content = content;
+    }
+
 }
 
